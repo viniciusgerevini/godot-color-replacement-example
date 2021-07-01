64d658e980a6d54d86619105b3a1c32cd8825c34
Vinicius Gerevini <viniciusgerevini@gmail.com>
2021-07-01 18:23:51 +1000

    extract color replacement shader to a file

--- scenes/example_with_shader.gd
+++ scenes/example_with_shader.gd
@@ -5,5 +5,5 @@ const Character = preload("res://scenes/characters/character_with_shader.tscn")
 
 func _create_character():
   var c = Character.instance()
-  c.material.set_shader_param("new", _random_color())
+  c.material.set_shader_param("new_color", _random_color())
   return c
