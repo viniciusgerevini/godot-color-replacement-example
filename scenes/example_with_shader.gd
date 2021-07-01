cf9b9076b1cc6add00cec40a35f5dc7b30d00947
Vinicius Gerevini <viniciusgerevini@gmail.com>
2021-07-01 18:17:43 +1000

    implemented shader example

---
+++ scenes/example_with_shader.gd
@@ -0,0 +1,9 @@
+extends "res://scenes/scene.gd"
+
+
+const Character = preload("res://scenes/characters/character_with_shader.tscn")
+
+func _create_character():
+  var c = Character.instance()
+  c.material.set_shader_param("new", _random_color())
+  return c
